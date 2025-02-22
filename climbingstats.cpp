f6a9c5f03b68211b8a4934c1cf229914cbb9a85d
Tamas K Stenczel <tks32@cam.ac.uk>
2025-02-22 22:11:18 +0000

    tmp

---
+++ climbingstats.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Tamas K Stenczel on 22/02/2025.
+//
+
+#include "climbingstats.h"
+
+#include <cstdio>
+
+
+void tmp_hello() {
+  printf("Hello World!\n");
+}
+
+
---
+++ climbingstats.h
@@ -0,0 +1,11 @@
+//
+// Created by Tamas K Stenczel on 22/02/2025.
+//
+
+#ifndef TMP_H
+#define TMP_H
+
+void tmp_hello();
+
+#endif //TMP_H
+
